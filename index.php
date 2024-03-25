2645a2b0908c035614138a6dfdbd69c0dea3cfea
Ikerarrazola <iker.arrazola@students.jmcss.org>
2024-03-25 08:07:37 -0500

    Update index.php

--- index.php
+++ index.php
@@ -5,14 +5,45 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>   
-<form method="GET">
-    <input type="text" name="person">
-    <button>SUBMIT</button>
-</form>    
-<?php 
-    $name = $_GET {'Person'};
-    echo $name. "is a bad boy!";       
-?>
+
+<body>
+
+<?php
+
+         //video 8 code
+         echo 5**5;
+
+        //video 9 code (Assignment Opreators)
+         $x = 100;
+         $x /= 20; 
+         
+         echo $x;
+
+         // video 10 code (Comparison Opreators)
+         $x = 10;
+         $y = 10;
+
+         if ($x == $y) {
+                echo "True!";
+         }
+         else {
+            echo "False!";
+         }
+
+         //video 11 code (Increment and Decrement Opreators)
+         $x = 10;
+         echo $x--;
+         echo $x;
+
+         //video 12 code (Logical Opreators)
+         $x = 20;
+         $y = 20;
+
+         if ($x == $y xor 1 == 1 ) {
+                echo "True";
+         }
+
+
+?>  
     </body>
 </html>
